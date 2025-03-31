8360157a17f883744ff8596eb7ca399898a7be98
valweq <zahiravalentinaortizsaavedra@gmail.com>
2025-03-30 21:52:30 -0500

    Val

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    let birthdayMessage = document.getElementById('birthdayMessage');
+    birthdayMessage.style.opacity = 1; // Hace que el mensaje aparezca
+};window.onload = function() {
+    let birthdayMessage = document.getElementById('birthdayMessage');
+    birthdayMessage.style.opacity = 1; // Hace que el mensaje aparezca
+}
